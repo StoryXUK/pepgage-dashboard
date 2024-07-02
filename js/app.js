a80c5b2b2f821e85a1820bfef71195777fc51b17
Responsibleuk <james@story-x.co.uk>
2024-07-02 14:38:56 +0000

    Add

--- js/app.js
+++ js/app.js
@@ -1,106 +1,141 @@
-/* Template Name: Ubold - Bootstrap 5 Landing Page Tamplat
-   Author: CoderThemes
-   File Description: Main JS file of the template
+/*
+Template Name: Kadso - Responsive Bootstrap 5 Admin Dashboard
+Author: Zoyothemes
+Version: 1.0.0
+Website: https://zoyothemes.com/
+File: Main Js File
 */
 
-// Sticky Navbar
 
-function windowScroll() {
-    const navbar = document.getElementById("sticky");
-    if (navbar) {
-        if (document.body.scrollTop >= 50 || document.documentElement.scrollTop >= 50) {
-            navbar.classList.add("nav-sticky");
-        } else {
-            navbar.classList.remove("nav-sticky");
+class App {
+
+    initComponents() {
+
+        // Waves Effect
+        Waves.init();
+
+        // Feather Icons
+        feather.replace()
+
+        // Popovers
+        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl)
+        })
+
+        // Tooltips
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
+
+        // Toasts
+        var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+        var toastList = toastElList.map(function (toastEl) {
+            return new bootstrap.Toast(toastEl)
+        })
+
+        // Toasts Placement
+        var toastPlacement = document.getElementById("toastPlacement");
+        if (toastPlacement) {
+            document.getElementById("selectToastPlacement").addEventListener("change", function () {
+                if (!toastPlacement.dataset.originalClass) {
+                    toastPlacement.dataset.originalClass = toastPlacement.className;
+                }
+                toastPlacement.className = toastPlacement.dataset.originalClass + " " + this.value;
+            });
+        }
+
+        // liveAlert
+        var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+        var alertTrigger = document.getElementById('liveAlertBtn')
+
+        function alert(message, type) {
+            var wrapper = document.createElement('div')
+            wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+            alertPlaceholder.append(wrapper)
+        }
+
+        if (alertTrigger) {
+            alertTrigger.addEventListener('click', function () {
+                alert('Nice, you triggered this alert message!', 'primary')
+            })
+        }
+
+        if (document.getElementById('app-style').href.includes('rtl.min.css')) {
+            document.getElementsByTagName('html')[0].dir = "rtl";
         }
     }
-}
 
-window.addEventListener("scroll", (ev) => {
-    ev.preventDefault();
-    windowScroll();
-});
+    initMenu() {
+        var self = this;
+        var body = document.body;
 
+        // Menu Toggle Button ( Placed in Topbar) (Left menu collapse)
+        var menuToggleBtn = document.querySelector('.button-toggle-menu');
+        if (menuToggleBtn) {
+            menuToggleBtn.addEventListener('click', function () {
 
-
-// back-to-top
-
-var mybutton = document.getElementById("back-to-top-btn");
-
-window.onscroll = function () {
-    scrollFunction();
-};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        console.log(document.body.scrollTop);
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-
-
-// Contact form
-
-function validateForm() {
-    var name = document.forms["myForm"]["name"].value;
-    var email = document.forms["myForm"]["email"].value;
-    var comments = document.forms["myForm"]["comments"].value;
-    document.getElementById("error-msg").style.opacity = 0;
-    document.getElementById("error-msg").innerHTML = "";
-    if (name == "" || name == null) {
-        document.getElementById("error-msg").innerHTML = "<div class='alert alert-danger error_message text-center'>Please enter a Name</div>";
-        fadeIn();
-        return false;
-    }
-    if (email == "" || email == null) {
-        document.getElementById("error-msg").innerHTML = "<div class='alert alert-danger error_message text-center'>Please enter a Email</div>";
-        fadeIn();
-        return false;
-    }
-    if (subject == "" || subject == null) {
-        document.getElementById("error-msg").innerHTML = "<div class='alert alert-danger error_message text-center'>Please enter a Subject</div>";
-        fadeIn();
-        return false;
-    }
-    if (comments == "" || comments == null) {
-        document.getElementById("error-msg").innerHTML = "<div class='alert alert-danger error_message text-center'>Please enter a Comments</div>";
-        fadeIn();
-        return false;
-    }
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("simple-msg").innerHTML = this.responseText;
-            document.forms["myForm"]["name"].value = "";
-            document.forms["myForm"]["email"].value = "";
-            document.forms["myForm"]["subject"].value = "";
-            document.forms["myForm"]["comments"].value = "";
+                if (body.getAttribute('data-sidebar') == 'default') {
+                    body.setAttribute('data-sidebar', 'hidden');
+                } else {
+                    body.setAttribute('data-sidebar', 'default');
+                }
+            });
         }
-    };
-    xhttp.open("POST", "php/contact.php", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send("name=" + name + "&email=" + email + "&comments=" + comments);
-    return false;
+
+        const updateOnWindowResize = () => {
+            if (window.innerWidth < 1040) {
+                body.setAttribute('data-sidebar', 'hidden');
+            } else {
+                body.setAttribute('data-sidebar', 'default');
+            }
+        }
+
+        updateOnWindowResize();
+        window.addEventListener('resize', updateOnWindowResize)
+
+        // sidebar - main menu
+        if ($("#side-menu").length) {
+            var navCollapse = $('#side-menu li .collapse');
+
+            // open one menu at a time only
+            navCollapse.on({
+                'show.bs.collapse': function (event) {
+                    var parent = $(event.target).parents('.collapse.show');
+                    $('#side-menu .collapse.show').not(parent).collapse('hide');
+                }
+            });
+
+            // activate the menu in left side bar (Vertical Menu) based on url
+            $("#side-menu a").each(function () {
+                var pageUrl = window.location.href.split(/[?#]/)[0];
+                if (this.href == pageUrl) {
+                    $(this).addClass("active");
+                    $(this).parent().addClass("menuitem-active");
+                    $(this).parent().parent().parent().addClass("show");
+                    $(this).parent().parent().parent().parent().addClass("menuitem-active");
+
+                    var firstLevelParent = $(this).parent().parent().parent().parent().parent().parent();
+                    if (firstLevelParent.attr('id') !== 'sidebar-menu') firstLevelParent.addClass("show");
+
+                    $(this).parent().parent().parent().parent().parent().parent().parent().addClass("menuitem-active");
+
+                    var secondLevelParent = $(this).parent().parent().parent().parent().parent().parent().parent().parent().parent();
+                    if (secondLevelParent.attr('id') !== 'wrapper') secondLevelParent.addClass("show");
+
+                    var upperLevelParent = $(this).parent().parent().parent().parent().parent().parent().parent().parent().parent().parent();
+                    if (!upperLevelParent.is('body')) upperLevelParent.addClass("menuitem-active");
+                }
+            });
+        }
+    }
+
+    init() {
+        this.initComponents();
+        this.initMenu();
+    }
 }
 
-function fadeIn() {
-    var fade = document.getElementById("error-msg");
-    var opacity = 0;
-    var intervalID = setInterval(function () {
-        if (opacity < 1) {
-            opacity = opacity + 0.5;
-            fade.style.opacity = opacity;
-        } else {
-            clearInterval(intervalID);
-        }
-    }, 200);
-}
+new App().init();
